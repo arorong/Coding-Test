0c24a71bb263917d4ba0a7920d88735dffd6a664
amy <168390622+arorong@users.noreply.github.com>
2025-08-25 10:56:11 +0900

    Update 합성수 찾기.java
    주석 추가

--- 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.java
@@ -2,18 +2,21 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
+        // 1부터 n까지 순회하면서 각 숫자의 약수 개수를 확인
         for(int i = 1; i <= n; i++){ 
-            int count = 0;
+            int count = 0; // 현재 숫자 i의 약수 개수를 저장
             
+            // 1부터 i까지 나눠서 i 약수 확인
             for(int j = 1; j <= i; j++){ 
-                if(i % j == 0){
+                if(i % j == 0){ // 나머지가 0이면 약수
                     count++;
                 } 
-            }         
+            }
+            // 약수의 개수가 3개 이상이면 합성수이므로 answer 증가
             if(count >= 3){ 
                 answer++;
             } 
         }
-        return answer;
+        return answer; // 합성수의 개수를 반환
     }
 }
