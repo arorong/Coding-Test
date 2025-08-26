b05375109e164c841b9d29f6edf490aadb440366
amy <168390622+arorong@users.noreply.github.com>
2025-08-26 10:57:58 +0900

    Update 외계행성의 나이.java

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,14 +1,13 @@
 class Solution {
     public String solution(int age) {
         String answer = "";
-        String str = String.valueOf(age);
-        
-        for (int i = 0; i < str.length(); i++) {
-            int num = str.charAt(i) - '0';
-            char ch = (char) ('a' + num);
-            answer += ch; 
+        String str = String.valueOf(age); //숫자를 문자열로
+ 
+        for (int i = 0; i < str.length(); i++) { //str 순회
+            int num = str.charAt(i) - '0'; //문자를 정수로 변환 예) 문자 '0'~'9' -> 정수 0~9
+            char ch = (char) ('a' + num); //숫자를 알파벳으로 변환 예) 정수 0~9 -> 문자 'a'~j'
+            answer += ch; //문자 이어붙이기
         }
-
         return answer;
     }
 }
