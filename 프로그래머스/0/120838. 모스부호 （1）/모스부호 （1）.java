634d0326dcb3576367357bf6643f837559610c43
amy <168390622+arorong@users.noreply.github.com>
2025-08-30 11:06:27 +0900

    [level 0] Title: 모스부호 (1), Time: 2.39 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String solution(String letter) {
+        Map<String, String> morseMap = new HashMap<>(){
+            {   put(".-","a");
+                put("-...","b");
+                put("-.-.","c");
+                put("-..","d");
+                put(".","e");
+                put("..-.","f");
+                put("--.","g");
+                put("....","h");
+                put("..","i");
+                put(".---","j");
+                put("-.-","k");
+                put(".-..","l");
+                put("--","m");
+                put("-.","n");
+                put("---","o");
+                put(".--.","p");
+                put("--.-","q");
+                put(".-.","r");
+                put("...","s");
+                put("-","t");
+                put("..-","u");
+                put("...-","v");
+                put(".--","w");
+                put("-..-","x");
+                put("-.--","y");
+                put("--..","z");
+             }
+        };
+        
+        String answer = "";
+        String[] morse = letter.split(" ");
+        for(String i : morse){
+            answer += morseMap.get(i);
+        }
+        
+        return answer;
+    }
+}
