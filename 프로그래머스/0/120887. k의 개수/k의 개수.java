62362dac8d73b4c3f8c6492af6bd6a76707672f3
amy <168390622+arorong@users.noreply.github.com>
2025-09-02 09:41:31 +0900

    [level 0] Title: k의 개수, Time: 3.87 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/0/120887. k의 개수/k의 개수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+
+        for (int a = i; a <= j; a++) {
+            int temp = a;
+
+            while (temp > 0) {
+                if (temp % 10 == k) {
+                    answer++;
+                }
+                temp /= 10;
+            }
+        }
+
+        return answer;
+    }
+}
