2857892d5e7694b644aceaccf26b819a1a78b10a
amy <168390622+arorong@users.noreply.github.com>
2025-09-03 10:16:28 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 4.09 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+       String[] sArr = s.split("");
+        Arrays.sort(sArr);
+        
+        for(int i = 0; i < sArr.length; i++){
+            int cnt = 0;
+            for(int j = 0; j < sArr.length; j++){
+                if(sArr[i].equals(sArr[j])){
+                    cnt++;
+                }
+            }
+            if(cnt == 1){
+                answer += sArr[i];
+            }
+        }
+        
+        return answer;
+    }
+}
