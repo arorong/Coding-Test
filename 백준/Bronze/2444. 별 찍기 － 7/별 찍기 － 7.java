21c6ff9806a765bb2989f36c37995ee895a7cdc3
amy <168390622+arorong@users.noreply.github.com>
2025-10-11 15:07:21 +0900

    Update 별 찍기 － 7.java

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -15,7 +15,7 @@ public class Main {
 			System.out.println();
     	}
     	
-    	for(int i = n-1; i >= 0; i--) {
+    	for(int i = n-1; i >= 1; i--) {
     		for(int j = 0; j < n-i; j++)
     			System.out.print(" ");
     		for(int k = 0; k < i*2-1; k++) {
@@ -24,4 +24,5 @@ public class Main {
     		System.out.println();
     	}
     }
+
 }
